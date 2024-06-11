cebfb43d0806d46f3488f303246d0a589bf60f29
Rafi-Rafique <160730669+Rafi-Rafique@users.noreply.github.com>
2024-06-12 00:22:00 +0500

    Add files via upload

---
+++ project-2-js/script.js
@@ -0,0 +1,116 @@
+// .............question-1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// .............question-2-INTEGERS
+
+// let num1 =parseInt(+prompt("Enter 1st integer"));
+// let num2 =parseInt(+prompt("Enter 2nd integer"));
+
+//     if (num1 > num2){
+//         console.log(num1,"is greater than",num2);
+//     }
+//     else if (num2 > num1){
+//         console.log(num2,"is greater than",num1);
+//     }
+//     else{
+//         console.log(num1,"and",num2,"are equal");
+//     }
+
+
+
+
+
+// .............question-3-NUMBER (positive, negative or zero)
+
+// let userNo =+prompt("Enter a number");
+
+// if(userNo >0){
+// console.log(userNo,"is positive number");
+// }
+// else if (userNo<0){
+//     console.log(userNo,"is negative number");
+// }
+// else {
+//     console.log(userNo,"is Zero");
+// }
+
+
+
+
+
+// .............question-4-VOWEL
+
+// let userChar =prompt("Enter a character");
+// let result =userChar;
+
+// if(userChar ==="a" || userChar ==="e" || userChar ==="i" || userChar ==="o"|| userChar ==="u"){
+// console.log("True", result,"is a vowel");
+// }
+// else{
+//     console.log("False", result,"is not a vowel");
+// }
+
+
+
+// .............question-5-PASSWORD
+
+// let checkPassword ="Saylani"
+// let userPassword=prompt("Enter your class password");
+
+// if (!userPassword){
+//     console.log("Enter your password");
+//     }
+// else if (userPassword === checkPassword){
+// console.log("Correct The password you entered matches the original password");
+// }
+// else{
+//     console.log("Incorrect Password");
+// }
+
+
+
+
+
+// .............question-7-GREETINGS
+
+// let hour =prompt("Enter your time in 24 hours clock format");
+
+// if(hour <=0){
+//    console.log("Enter valid time"); 
+
+// }
+// else if(hour <0 && hour <= 11){
+//     console.log("Good Morning"); 
+ 
+//  }
+//  else if(hour >=12 && hour <=16){
+//     console.log("Good Afternoon"); 
+ 
+//  }
+//  else if(hour >=17 && hour <=20){
+//     console.log("Good Evening"); 
+ 
+//  }
+//  else if(hour >=21 && hour <=24){
+//     console.log("Good Night"); 
+ 
+//  }
+//  else {
+//     console.log("Invalid Time"); 
+ 
+//  }
+
+
